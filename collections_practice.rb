57b82999ba5576b15c1d0755449be02aa45541ff
Steve Albonico <salbonico@gmail.com>
2019-01-16 00:54:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,8 @@ def sort_array_desc(array)
 new_array = array.sort {|a,b|  b <=> a}
 return new_array
 end
+
+def sort_array_char_count(array)
+new_array = array.sort {|a.length,b.length|  a <=> b}
+return new_array
+end
