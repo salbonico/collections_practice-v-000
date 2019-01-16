448815b1db53220f8afa09efb43107f43a0775d4
Steve Albonico <salbonico@gmail.com>
2019-01-16 01:00:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,3 +12,10 @@ def sort_array_char_count(array)
 new_array = array.sort {|a,b|  a.length <=> b.length}
 return new_array
 end
+
+def swap_elements(array)
+new_array = []
+new_array.push(array[0])
+new_array.push(array[2])
+new_array.push(array[1])
+end
